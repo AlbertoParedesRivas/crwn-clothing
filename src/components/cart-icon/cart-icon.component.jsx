ad172108170f1117d4de0c36cfc29d496b0d985d
Alberto Paredes <alberto.paredes.bs@gmail.com>
2022-06-30 19:23:08 -0500

    Functionality to add items to cart and styling

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import { CartContext } from "../../context/cart.context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { cartCount, isCartOpen, setIsCartOpen } = useContext(CartContext);
 
     const toggleCartDropdown = () => {
         setIsCartOpen(!isCartOpen);
@@ -15,7 +15,7 @@ const CartIcon = () => {
     return (
         <div className="cart-icon-container" onClick={toggleCartDropdown}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{cartCount}</span>
         </div>
     );
 };
